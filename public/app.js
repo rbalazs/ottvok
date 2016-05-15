43ef984736fc664ccc3f4ddc3f9e3fdcdb5e0cba
Richer Balázs <richer.balazs@gmail.com>
2016-05-15 13:26:01 +0200

    TripModel model class based on Google Maps Distance Matrix API.

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 requirejs.config({
     paths: {
         'knockout-3.4.0' : 'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.0/knockout-min',
-        'jquery' : 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min'
+        'jquery' : 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min',
+        'tripModel' : 'scripts/model/tripModel'
     }
 });
 
-require(['knockout-3.4.0', 'jquery'], function(ko, $) {
+require(['knockout-3.4.0', 'jquery', 'tripModel'], function(ko, $, tripModel) {
+    var trip = new tripModel();
 
+    ko.applyBindings(trip);
 });
---
+++ public/scripts/model/tripModel.js
@@ -0,0 +1,9 @@
+define(['knockout-3.4.0'], function (ko) {
+    return function() {
+        this.destinationAddresses = ko.observable();
+        this.originAddresses = ko.observable();
+        this.distance = ko.observable();
+        this.duration = ko.observable();
+        this.status = ko.observable();
+    };
+});
