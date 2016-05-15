154df31af1be9d05e4ad70e28582670bb9a31a5d
Richer Balázs <richer.balazs@gmail.com>
2016-05-15 14:28:48 +0200

    Get distance and eta based on Google Maps Distance Matrix APIm with server side proxy.

--- server.js
+++ server.js
@@ -8,4 +8,37 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/distancematrix/*', function (req, res) {
+    var https = require('https');
+    var fs = require('fs');
+    var key = '';
+
+    fs.readFile('apikey', function (err, content) {
+        if (err) {
+            console.log('Error loading client secret file (apikey): ' + err);
+            return;
+        }
+
+        key = content;
+
+        var options = {
+            host: 'maps.googleapis.com',
+            path: '/maps/api/distancematrix/json?origins=r%C3%A1kosfalva%20park&destinations=m%C3%A9sz%C3%A1ros%20utca&key=' + key,
+            port: 443,
+            method: 'GET'
+        };
+
+        var body = '';
+
+        https.request(options, function (response) {
+            response.on('data', function (chunk) {
+                body += chunk;
+            });
+            response.on('end', function () {
+                res.json(JSON.parse(body));
+            });
+        }).end();
+    });
+});
+
 app.listen(PORT);
