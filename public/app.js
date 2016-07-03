49a3f5427617ef9d0c4c5dffe6227150a1fbfcdc
Richer Balázs <richer.balazs@gmail.com>
2016-07-03 16:23:03 +0200

    GH-15 - Highlight best departure time.

--- public/app.js
+++ public/app.js
@@ -1,87 +1,100 @@
 requirejs.config({
-    shim: {
-        "bootstrap": {"deps": ['jquery']}
-    },
-    paths: {
-        'async': 'https://cdnjs.cloudflare.com/ajax/libs/requirejs-async/0.1.1/async',
-        'knockout-3.4.0': 'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.0/knockout-min',
-        'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min',
-        'moment': 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min',
-        'TripModel': 'scripts/model/TripModel',
-        'ProvisionModel': 'scripts/model/ProvisionModel',
-        'bootstrap': 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min',
-        'utils': 'scripts/helper/utils'
+  shim: {
+    "bootstrap": {
+      "deps": ['jquery']
     }
+  },
+  paths: {
+    'async': 'https://cdnjs.cloudflare.com/ajax/libs/requirejs-async/0.1.1/async',
+    'knockout-3.4.0': 'https://cdnjs.cloudflare.com/ajax/libs/knockout/3.4.0/knockout-min',
+    'jquery': 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min',
+    'moment': 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.13.0/moment.min',
+    'TripModel': 'scripts/model/TripModel',
+    'ProvisionModel': 'scripts/model/ProvisionModel',
+    'bootstrap': 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min',
+    'utils': 'scripts/helper/utils'
+  }
 });
 
-require(['knockout-3.4.0', 'jquery', 'TripModel', 'ProvisionModel', 'utils', 'moment', 'async!https://maps.googleapis.com/maps/api/js?key=AIzaSyC_ZRibN_wYmc-bhLP7L8saRMoitVuUVFU&libraries=places'],
-    function (ko, $, TripModel, ProvisionModel, utils, moment) {
-        function doProvision(trip, minutesFromNowArg) {
-            var minutesFromNow = minutesFromNowArg || 0;
-            var baseUrl = 'distancematrix/getJSONWithProxy/';
-            var url = baseUrl + trip.originAddresses() + '/' + trip.destinationAddresses() + '/';
-            var timestamp = utils.getTimestampWithAdditionalMinutes(minutesFromNow);
-            var provision = new ProvisionModel();
+require(['knockout-3.4.0', 'jquery', 'TripModel', 'ProvisionModel', 'utils',
+    'moment',
+    'async!https://maps.googleapis.com/maps/api/js?key=AIzaSyC_ZRibN_wYmc-bhLP7L8saRMoitVuUVFU&libraries=places'
+    ],
+  function (ko, $, TripModel, ProvisionModel, utils, moment) {
+    function doProvision(trip, minutesFromNowArg) {
+      var minutesFromNow = minutesFromNowArg || 0;
+      var baseUrl = 'distancematrix/getJSONWithProxy/';
+      var url = baseUrl + trip.originAddresses() + '/' + trip.destinationAddresses() +
+        '/';
+      var timestamp = utils.getTimestampWithAdditionalMinutes(minutesFromNow);
+      var provision = new ProvisionModel();
 
-            // Fetch google api datas from a backend proxy, to avoid cross-origin.
-            $.getJSON(url + timestamp, function (data) {
-                trip.originAddresses(data.origin_addresses);
-                trip.distance(data.rows[0].elements[0].distance.text);
-                trip.status(data.status);
-                provision.durationInTraffic(data.rows[0].elements[0].duration_in_traffic.text);
-                provision.timestampVerbose(moment.unix(timestamp).fromNow() + " (" + moment.unix(timestamp).format('H:mm') + ")");
-                provision.timestamp(timestamp);
-                $('.loader').hide();
-            });
+      // Fetch google api datas from a backend proxy, to avoid cross-origin.
+      $.getJSON(url + timestamp, function (data) {
+        trip.originAddresses(data.origin_addresses);
+        trip.distance(data.rows[0].elements[0].distance.text);
+        trip.status(data.status);
+        provision.durationInTraffic(data.rows[0].elements[0].duration_in_traffic
+          .text);
+        provision.timestampVerbose(moment.unix(timestamp).fromNow() +
+          " (" + moment.unix(timestamp).format('H:mm') + ")");
+        provision.timestamp(timestamp);
+        $('.loader').hide();
+      });
 
-            trip.provisions.push(provision);
-        }
-
-        $(document).ready(function () {
-            var trip = new TripModel();
-            var optionsAS = {
-                types: ['address'],
-                componentRestrictions: {
-                    country: 'hu'
-                }
-            };
-
-            var minutesAddedList = [5, 15, 30, 45, 60, 90, 120, 150, 180];
-
-            var originAddressesAS = new google.maps.places.Autocomplete(
-                document.getElementById('originAddresses'),
-                optionsAS
-            );
-
-            var destinationAddressesAS = new google.maps.places.Autocomplete(
-                document.getElementById('destinationAddresses'),
-                optionsAS
-            );
-
-            google.maps.event.addListener(originAddressesAS, 'place_changed', function () {
-                trip.originAddresses(originAddressesAS.getPlace().formatted_address);
-            });
-
-            google.maps.event.addListener(destinationAddressesAS, 'place_changed', function () {
-                trip.destinationAddresses(destinationAddressesAS.getPlace().formatted_address);
-            });
-
-            ko.applyBindings(trip);
-
-            $('.do-provision').click(function () {
-                $('.loader').show();
-                if (typeof trip.originAddresses() === 'undefined') {
-                    trip.originAddresses('rákosfalva park');
-                }
-                if (typeof trip.destinationAddresses() === 'undefined') {
-                    trip.destinationAddresses('mészáros utca');
-                }
-                for (minutesAddedIndex in minutesAddedList) {
-                    if (minutesAddedList.hasOwnProperty(minutesAddedIndex)) {
-                      doProvision(trip, minutesAddedList[minutesAddedIndex]);
-                    }
-                }
-            });
-        });
+      trip.provisions.push(provision);
     }
+
+    $(document).ready(function () {
+      var trip = new TripModel();
+      var optionsAS = {
+        types: ['address'],
+        componentRestrictions: {
+          country: 'hu'
+        }
+      };
+
+      var minutesAddedList = [5, 15, 30, 45, 60, 90, 120, 150, 180];
+
+      var originAddressesAS = new google.maps.places.Autocomplete(
+        document.getElementById('originAddresses'),
+        optionsAS
+      );
+
+      var destinationAddressesAS = new google.maps.places.Autocomplete(
+        document.getElementById('destinationAddresses'),
+        optionsAS
+      );
+
+      ko.applyBindings(trip);
+
+      google.maps.event.addListener(originAddressesAS, 'place_changed',
+        function () {
+          trip.originAddresses(originAddressesAS.getPlace().formatted_address);
+        });
+
+      google.maps.event.addListener(destinationAddressesAS,
+        'place_changed',
+        function () {
+          trip.destinationAddresses(destinationAddressesAS.getPlace().formatted_address);
+        });
+
+      $('.do-provision').click(function () {
+        trip.provisions.removeAll();
+        $('.loader').show();
+        if (typeof trip.originAddresses() === 'undefined') {
+          trip.originAddresses('rákosfalva park');
+        }
+        if (typeof trip.destinationAddresses() === 'undefined') {
+          trip.destinationAddresses(
+            'Budapest Mészáros 1016 Magyarország');
+        }
+        for (minutesAddedIndex in minutesAddedList) {
+          if (minutesAddedList.hasOwnProperty(minutesAddedIndex)) {
+            doProvision(trip, minutesAddedList[minutesAddedIndex]);
+          }
+        }
+      });
+    });
+  }
 );
--- public/scripts/model/TripModel.js
+++ public/scripts/model/TripModel.js
@@ -1,12 +1,29 @@
 define(['knockout-3.4.0'], function (ko) {
-    /**
-     * Model class for a trip.
-     */
-    return function TripModel() {
-        this.destinationAddresses = ko.observable();
-        this.originAddresses = ko.observable();
-        this.distance = ko.observable();
-        this.status = ko.observable();
-        this.provisions = ko.observableArray();
-    };
+  /**
+   * Model class for a trip.
+   */
+  return function TripModel() {
+    var self = this;
+    self.destinationAddresses = ko.observable();
+    self.originAddresses = ko.observable();
+    self.distance = ko.observable();
+    self.status = ko.observable();
+    self.provisions = ko.observableArray();
+    self.fastestProvision = ko.pureComputed(function () {
+      if (self.provisions().length == 0) {
+        return {};
+      }
+      return self.provisions().reduce(function (
+        previousValue,
+        currentValue,
+        currentIndex,
+        array
+      ) {
+        return parseInt(currentValue.durationInTraffic()) <
+          parseInt(previousValue.durationInTraffic()) ?
+          currentValue :
+          previousValue;
+      });
+    })
+  };
 });
